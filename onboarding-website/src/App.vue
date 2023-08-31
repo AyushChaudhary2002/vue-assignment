9b390bfab82aa9ab7852a5141364173901fed117
ayush2002-de <ayush.kumar0209@gmail.com>
2023-08-31 09:43:02 +0530

    vue-assignment-v1: UI design.

---
+++ onboarding-website/src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import navbarComponent from '@/components/navbarComponent.vue'
+import asideComponent from '@/components/asideComponent.vue'
+import sectionComponent from './components/sectionComponent.vue';
+</script>
+
+<template>
+  <header class="header">
+    <navbarComponent />
+  </header>
+  <main class="main">
+    <aside class="aside">
+      <asideComponent/>
+    </aside>
+    <section class="section">
+      <sectionComponent/>
+    </section>
+  </main>
+</template>
+
+<style scoped>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: sans-serif;
+}
+.header{
+  width: 100vw;
+  background-color: #673ab5;
+}
+.main{
+  display: flex;
+  height: 79vh;
+  margin: 1vh 0vh;
+}
+.aside{
+  width: 20vw;
+  margin:1rem;
+  padding: 0.5rem;
+}
+.section{
+  width:70vw;
+  margin:1rem;
+  padding: 0.5rem;
+}
+</style>
